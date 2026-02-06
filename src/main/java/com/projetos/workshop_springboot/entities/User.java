5d61af50f47fdc218f39fdeba445439809ccfb38
Vinicius <vinicius@lambdatt.com.br>
2026-02-05 23:37:05 -0300

    Configuração do H2 e Mapeamento do User

--- src/main/java/com/projetos/workshop_springboot/entities/User.java
+++ src/main/java/com/projetos/workshop_springboot/entities/User.java
@@ -3,10 +3,20 @@ package com.projetos.workshop_springboot.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user") // User é reservado do BD H2
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
