3a18548887610388d9b86a6816fdd7b2ef654a56
Vinicius <vinicius@lambdatt.com.br>
2026-02-06 16:44:04 -0300

    Payment OneToOne

--- src/main/java/com/projetos/workshop_springboot/config/TestConfig.java
+++ src/main/java/com/projetos/workshop_springboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetos.workshop_springboot.entities.Category;
 import com.projetos.workshop_springboot.entities.Order;
 import com.projetos.workshop_springboot.entities.OrderItem;
+import com.projetos.workshop_springboot.entities.Payment;
 import com.projetos.workshop_springboot.entities.Product;
 import com.projetos.workshop_springboot.entities.User;
 import com.projetos.workshop_springboot.entities.enums.OrderStatus;
@@ -46,7 +47,7 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.DELIVERED);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT); 
 		
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		// Salvar um objeto dependente em uma relação OneToOne
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 
 		
