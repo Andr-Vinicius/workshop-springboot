f6d8f4b21f7f108947718b652a84c901e4074760
Vinicius <vinicius@lambdatt.com.br>
2026-02-06 01:43:20 -0300

    FindAll e FindById

--- src/main/java/com/projetos/workshop_springboot/resources/UserResource.java
+++ src/main/java/com/projetos/workshop_springboot/resources/UserResource.java
@@ -1,21 +1,35 @@
 package com.projetos.workshop_springboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetos.workshop_springboot.entities.User;
+import com.projetos.workshop_springboot.services.UserService;
 
 @RestController // Definição de que será o Controller
 @RequestMapping(value = "/users") // Endpoint
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	// Mapeamento do Usuário
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "teste@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 
 }
